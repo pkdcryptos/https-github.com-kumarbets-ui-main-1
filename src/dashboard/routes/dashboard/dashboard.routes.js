4a44e4cb29dbb9b173cbceca9d755a00c1a2b6f0
kumarbets <kumarbets@gmail.com>
2024-08-10 11:17:36 -0700

    sfsg - contacts removed

--- src/dashboard/routes/dashboard/dashboard.routes.js
+++ src/dashboard/routes/dashboard/dashboard.routes.js
@@ -1,8 +1,7 @@
 import AppContainer from './Dashboard';
 import settings from './settings/settings.routes';
 import conversation from './conversation/conversation.routes';
-import { routes as contactRoutes } from './contacts/routes';
-import { routes as notificationRoutes } from './notifications/routes';
+
 import { frontendURL } from '../../helper/URLHelper';
 
 export default {
@@ -13,8 +12,6 @@ export default {
       children: [
         ...conversation.routes,
         ...settings.routes,
-        ...contactRoutes,
-        ...notificationRoutes,
       ],
     },
   ],
